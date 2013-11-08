49165eddbf0a5842aea6d556ad055aa43f79566e
Moiri Gamboni <Moiri Gamboni>
2013-11-08 18:02:45 +0400

    Finished wrapper code for double half hulls

--- src/hs_convex_hull/Hull.java
+++ src/hs_convex_hull/Hull.java
@@ -18,18 +18,29 @@ public class Hull {
 		HalfHull.showHull(img, hull[1].inspect(), hull[1].points, true);
 	}
 
+	public Chain[] inspect(){
+		Chain[] hullChain = new Chain[2];
+		hullChain[0] = hull[0].inspect();
+		hullChain[1] = hull[1].inspect();
+		return hullChain;
+	}
+
 	public void delete(Point p){
 		int idx = Arrays.binarySearch(hull[0].points, p);
+		System.out.println(idx);
 		hull[0].delete(p);
-		hull[1].delete(hull[1].points[hull[0].points.length-idx]);
+		hull[1].delete(hull[1].points[hull[0].points.length-idx-1]);
 	}
 	
 
 	public static void main(String[] args) {
-		int size = 16;
+		int size = 17;
 		PointDistribution ps = new PointDistribution(size);
 		Hull hull = new Hull(ps.normalPoints);
 		Polygonization img = new Polygonization(false);
+		hull.showHull(img);
+		hull.delete(hull.hull[0].points[0]);
+		img = new Polygonization(false);
 		hull.showHull(img);
 	}
 }
